bf4611b8e8e5550927798ef6306bda4ecb6bcbb4
shruthivis <shruthivis@users.noreply.github.com>
2018-11-03 17:06:12 -0700

    Update tfiih_list_of_steps.sh

--- scripts/tfiih/tfiih_list_of_steps.sh
+++ scripts/tfiih/tfiih_list_of_steps.sh
@@ -1,44 +1,43 @@
+## Iteration 1 (Coarse-grain regions without known structure to 10 residues per bead).
 
-~/imp-clean/build/setup_environment.sh python ~/imp-clean/imp/modules/optrep/pyext/src/set_next_beadmap.py -u create -r 10 -tf ~/optrep/input/tfiih/tfiih_topology.txt
+# 1. Create beadmap
+~/imp-clean/build/setup_environment.sh python ~/imp-clean/imp/modules/optrep/pyext/src/set_next_beadmap.py -u create -r 10 -tf 
+~/optrep/input/tfiih/tfiih_topology.txt
 
-Sample initially
-
-For full runs:
-7000 steps at 50 runs
-
-sh ~/optrep/code/scripts/tfiih/job_sample_wrapper.sh 30 7000 50 labq
-
-For quick run (to get ropt):
-3500 steps at 24 runs
-
+# 2. Sample models in 10 residues/bead representation 
 sh ~/optrep/code/scripts/tfiih/job_sample_wrapper.sh 10 3500 24 labq
 
-Trying lab.q: it takes a couple of hours! 
+# 3. Get good-scoring models from sampling
+# (use total score to get a few thousand models)
+date ; ~/imp-clean/build/setup_environment.sh python ~/imp-clean/imp/modules/optrep/pyext/src/select_good_scoring_models.py -rd ./ 
+-rp run. -cl Total_Score -kl Total_Score -agl -99999.9 -aul -110 -mlt 0.0 -mut 0.0 ; date
+# (takes 20 mins)
 
-3. Get gsms:
-(use total score to get a few thousand models)
-date ; ~/imp-clean/build/setup_environment.sh python ~/imp-clean/imp/modules/optrep/pyext/src/select_good_scoring_models.py -rd ./ -rp run. -cl Total_Score -kl Total_Score -agl -99999.9 -aul -110 -mlt 0.0 -mut 0.0 ; date
+# 4. Get bead-wise sampling precision
+sh ~/optrep/code/scripts/tfiih/precision_wrapper.sh 50 10 3500
 
-(takes 20 mins)
+# 5. Concatenate the beadmaps from different cores 
+for i in `seq 0 49`; do cat bead_precisions_sub.$i >> bead_precisions_10.txt; done 
 
-4. Get sampling precision of bm domains 
-Job_precision.sh 
-Which contains only domains subject to change (beadmap)
-
-Cat … 
-5. Get next level beadmap (r=?)
+## Iteration 2 (Coarse-grain imprecise beads to 30 residues per bead).
+# 6. Get updated beadmap 
 ~/imp-clean/build/setup_environment.sh python ~/imp-clean/imp/modules/optrep/pyext/src/set_next_beadmap.py -u update -r 30 -bmf r10/bead_map_10.txt -pf r10/bead_precisions_10.txt
 
-6. Sample again
+# 7. Sample models
 sh ~/optrep/code/scripts/tfiih/job_sample_wrapper.sh 30 3500 24 labq 
 
-7. Get gsms:
+# 8. Get good-scoring models from sampling
 (use total score to get a few thousand models)
 date ; ~/imp-clean/build/setup_environment.sh python ~/imp-clean/imp/modules/optrep/pyext/src/select_good_scoring_models.py -rd ./ -rp run. -cl Total_Score -kl Total_Score -agl -99999.9 -aul -144 -mlt 0.0 -mut 0.0 ; date
 
-8. Job_precision.sh
+# 9. Get bead-wise sampling precision
+sh ~/optrep/code/scripts/tfiih/precision_wrapper.sh 50 30 3500
 
-9. ~/imp-clean/build/setup_environment.sh python ~/imp-clean/imp/modules/optrep/pyext/src/set_next_beadmap.py -u update -r 50 -bmf r30/bead_map_30.txt -pf r30/bead_precisions_30.txt
+# 10. Concatenate the beadmaps from different cores 
+for i in `seq 0 49`; do cat bead_precisions_sub.$i >> bead_precisions_30.txt; done 
 
-10. Final sampling
+## Iteration 3 (Coarse-grain imprecise beads to 50 residues per bead).
+ ~/imp-clean/build/setup_environment.sh python ~/imp-clean/imp/modules/optrep/pyext/src/set_next_beadmap.py -u update -r 50 -bmf r30/bead_map_30.txt -pf r30/bead_precisions_30.txt
+
+# Perform complete sampling
 sh ~/optrep/code/scripts/tfiih/job_sample_wrapper.sh 50 7000 50 labq 
